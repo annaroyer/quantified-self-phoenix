7e5a15b68155e88c7e70a9a166c83177e9dbebb9
Anna Royer <anna.royer@colorado.edu>
2018-05-09 22:03:35 -0600

    add controllers directory and foods controller

---
+++ lib/qs_phoenix_web/controllers/fallback_controller.ex
@@ -0,0 +1,18 @@
+defmodule QsPhoenixWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use QsPhoenixWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+  end
+end
