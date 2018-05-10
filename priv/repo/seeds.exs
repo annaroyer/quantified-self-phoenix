789cdb09aee362bed6f311bed6e9b0c002759b97
Anna Royer <anna.royer@colorado.edu>
2018-05-10 11:22:13 -0600

    update seeds file to seed 7 foods

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,7 +5,24 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     QsPhoenix.Repo.insert!(%QsPhoenix.SomeSchema{})
+#     QsPhoenix.QsPhoenix.Repo.insert!(%QsPhoenix.SomeSchema{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+defmodule QsPhoenix.DatabaseSeeder do
+  alias QsPhoenix.Foods
+
+  @foods [%{name: "Banana",calories: 150},
+          %{name: "Bagel Bites - Four Cheese", calories: 650},
+          %{name: "Chicken Burrito", calories: 800},
+          %{name: "Yogurt", calories: 550},
+          %{name: "Gum", calories: 50},
+          %{name: "Cheese", calories: 400},
+          %{name: "Apple", calories: 220}]
+
+  def seed_foods do
+    Enum.each(@foods, fn(food) -> Foods.create_food(food) end )
+  end
+end
+
+QsPhoenix.DatabaseSeeder.seed_foods()
