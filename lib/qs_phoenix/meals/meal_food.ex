747e1f7961b5c939ff5bd5daa72df028bd1e88ba
Anna Royer <anna.royer@colorado.edu>
2018-05-12 23:47:59 -0600

    update meal changeset to include neccessary attributes for meal_food create

--- lib/qs_phoenix/meals/meal_food.ex
+++ lib/qs_phoenix/meals/meal_food.ex
@@ -15,7 +15,7 @@ defmodule QsPhoenix.Meals.MealFood do
   @doc false
   def changeset(meal_food, attrs) do
     meal_food
-    |> cast(attrs, [])
+    |> cast(attrs, [:meal_id, :food_id])
     |> validate_required([])
   end
 end
