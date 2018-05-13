92a6f65762b49d261e9447927b964f867479bd75
Anna Royer <anna.royer@colorado.edu>
2018-05-12 19:59:15 -0600

    add many to many relationship to meals

--- lib/qs_phoenix/foods/food.ex
+++ lib/qs_phoenix/foods/food.ex
@@ -2,12 +2,16 @@ defmodule QsPhoenix.Foods.Food do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias QsPhoenix.Meals.Meal
+
 
   schema "foods" do
     field :calories, :integer
     field :name, :string
 
     timestamps()
+
+    many_to_many :meals, Meal, join_through: "meal_foods"
   end
 
   @doc false
