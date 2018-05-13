9009dc04e5fe823bb5dad7882e9f0cdf1ce1431e
Anna Royer <anna.royer@colorado.edu>
2018-05-12 23:52:22 -0600

    test post food to path

--- test/qs_phoenix_web/controllers/meal_food_controller_test.exs
+++ test/qs_phoenix_web/controllers/meal_food_controller_test.exs
@@ -4,34 +4,37 @@ defmodule QsPhoenixWeb.MealFoodControllerTest do
   # alias QsPhoenix.Meals
   # alias QsPhoenix.Meals.MealFood
   #
-  # @create_attrs %{}
-  # @update_attrs %{}
-  # @invalid_attrs %{}
-  #
+  @create_attrs %{meal_id: 1, food_id: 4}
+
   # def fixture(:meal_food) do
   #   {:ok, meal_food} = Meals.create_meal_food(@create_attrs)
   #   meal_food
   # end
-  #
-  # setup %{conn: conn} do
-  #   {:ok, conn: put_req_header(conn, "accept", "application/json")}
-  # end
-  #
-  # describe "create meal_food" do
-  #   test "renders meal_food when data is valid", %{conn: conn} do
-  #     conn = post conn, meal_food_path(conn, :create), meal_food: @create_attrs
-  #     assert %{"id" => id} = json_response(conn, 201)["data"]
-  #
-  #     conn = get conn, meal_food_path(conn, :show, id)
-  #     assert json_response(conn, 200)["data"] == %{
-  #       "id" => id}
-  #   end
-  #
-  #   test "renders errors when data is invalid", %{conn: conn} do
-  #     conn = post conn, meal_food_path(conn, :create), meal_food: @invalid_attrs
-  #     assert json_response(conn, 422)["errors"] != %{}
-  #   end
-  # end
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "create meal_food" do
+    test "renders meal_food when data is valid", %{conn: conn} do
+      conn = post conn, meal_food_path(conn, :create, 1, 4)
+      assert %{"message" => "Successfully added Yogurt to Breakfast"} = json_response(conn, 201)
+
+      new_meal_food = %{ "id" => 4, "name" => "Yogurt", "calories" => 550}
+      conn = get conn, meal_path(conn, :show, 1)
+      assert List.last(json_response(conn, 200)["foods"]) == new_meal_food
+    end
+
+    test "renders a 404 when meal or food with given ids DNE", %{conn: conn} do
+      assert_error_sent :not_found, fn ->
+        post conn, meal_food_path(conn, :create, 5, 1)
+      end
+
+      assert_error_sent :not_found, fn ->
+        post conn, meal_food_path(conn, :create, 1, 8)
+      end
+    end
+  end
   #
   # describe "delete meal_food" do
   #   setup [:create_meal_food]
