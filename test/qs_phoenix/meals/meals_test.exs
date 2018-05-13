d257546a603bf9aa534d0b195d55f878d34f02de
Anna Royer <anna.royer@colorado.edu>
2018-05-13 00:01:08 -0600

    add validation tests for create meal

--- test/qs_phoenix/meals/meals_test.exs
+++ test/qs_phoenix/meals/meals_test.exs
@@ -25,12 +25,12 @@ defmodule QsPhoenix.MealsTest do
     # end
   end
 
-  # describe "meal_foods" do
-  #   alias QsPhoenix.Meals.MealFood
-  #
-  #   @valid_attrs %{}
-  #   @update_attrs %{}
-  #   @invalid_attrs %{}
+  describe "meal_foods" do
+    alias QsPhoenix.Meals.MealFood
+
+    @valid_attrs %{meal_id: 1, food_id: 7}
+    @update_attrs %{}
+    @invalid_attrs %{}
   #
   #   def meal_food_fixture(attrs \\ %{}) do
   #     {:ok, meal_food} =
@@ -40,37 +40,17 @@ defmodule QsPhoenix.MealsTest do
   #
   #     meal_food
   #   end
-  #
-  #   test "list_meal_foods/0 returns all meal_foods" do
-  #     meal_food = meal_food_fixture()
-  #     assert Meals.list_meal_foods() == [meal_food]
-  #   end
-  #
-  #   test "get_meal_food!/1 returns the meal_food with given id" do
-  #     meal_food = meal_food_fixture()
-  #     assert Meals.get_meal_food!(meal_food.id) == meal_food
-  #   end
-  #
-  #   test "create_meal_food/1 with valid data creates a meal_food" do
-  #     assert {:ok, %MealFood{} = meal_food} = Meals.create_meal_food(@valid_attrs)
-  #   end
-  #
-  #   test "create_meal_food/1 with invalid data returns error changeset" do
-  #     assert {:error, %Ecto.Changeset{}} = Meals.create_meal_food(@invalid_attrs)
-  #   end
-  #
-  #   test "update_meal_food/2 with valid data updates the meal_food" do
-  #     meal_food = meal_food_fixture()
-  #     assert {:ok, meal_food} = Meals.update_meal_food(meal_food, @update_attrs)
-  #     assert %MealFood{} = meal_food
-  #   end
-  #
-  #   test "update_meal_food/2 with invalid data returns error changeset" do
-  #     meal_food = meal_food_fixture()
-  #     assert {:error, %Ecto.Changeset{}} = Meals.update_meal_food(meal_food, @invalid_attrs)
-  #     assert meal_food == Meals.get_meal_food!(meal_food.id)
-  #   end
-  #
+
+    test "create_meal_food/1 with valid data creates a meal_food" do
+      assert {:ok, %MealFood{} = meal_food} = Meals.create_meal_food(@valid_attrs)
+    end
+
+    test "create_meal_food/1 with invalid data returns error changeset" do
+      assert_raise Postgrex.Error, fn ->
+        Meals.create_meal_food(@invalid_attrs)
+      end
+    end
+
   #   test "delete_meal_food/1 deletes the meal_food" do
   #     meal_food = meal_food_fixture()
   #     assert {:ok, %MealFood{}} = Meals.delete_meal_food(meal_food)
@@ -81,5 +61,5 @@ defmodule QsPhoenix.MealsTest do
   #     meal_food = meal_food_fixture()
   #     assert %Ecto.Changeset{} = Meals.change_meal_food(meal_food)
   #   end
-  # end
+  end
 end
