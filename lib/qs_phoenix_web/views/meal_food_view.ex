ba778446b354ea728d2da19a654e333f4d932d4e
Anna Royer <anna.royer@colorado.edu>
2018-05-12 23:51:34 -0600

    remove unnecessary rendering in meal_food_view and add message render

--- lib/qs_phoenix_web/views/meal_food_view.ex
+++ lib/qs_phoenix_web/views/meal_food_view.ex
@@ -2,15 +2,7 @@ defmodule QsPhoenixWeb.MealFoodView do
   use QsPhoenixWeb, :view
   alias QsPhoenixWeb.MealFoodView
 
-  def render("index.json", %{meal_foods: meal_foods}) do
-    %{data: render_many(meal_foods, MealFoodView, "meal_food.json")}
-  end
-
-  def render("show.json", %{meal_food: meal_food}) do
-    %{data: render_one(meal_food, MealFoodView, "meal_food.json")}
-  end
-
-  def render("meal_food.json", %{meal_food: meal_food}) do
-    %{id: meal_food.id}
+  def render("show.json", %{message: message}) do
+    %{message: message}
   end
 end
