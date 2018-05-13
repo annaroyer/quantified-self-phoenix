e76eb5f15fc281358f3bde0902052814cf3230e0
Anna Royer <anna.royer@colorado.edu>
2018-05-13 02:26:22 -0600

    delete unused variables

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,17 +1,4 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     QsPhoenix.QsPhoenix.Repo.insert!(%QsPhoenix.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
 defmodule QsPhoenix.DatabaseSeeder do
-  import Ecto.Changeset
-  alias QsPhoenix.Foods
   alias QsPhoenix.Meals.Meal
   alias QsPhoenix.Foods.Food
   alias QsPhoenix.Meals.MealFood
